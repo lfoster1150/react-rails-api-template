e733396dd996938915dc7ee50afc8def78c33552
Luke Foster <lfoster1150@gmail.com>
2024-02-27 11:07:15 -0600

    Fix controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  resopond_to :json
+  respond_to :json
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
